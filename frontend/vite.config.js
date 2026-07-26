2bc4e64986dc6bfe533c04655e589261451d7f01
Harsh Maker <harsh@example.com>
2026-07-26 16:58:32 +0530

    Revert output dir back to dist for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'build'
-  }
+  plugins: [react(), tailwindcss()]
 })
